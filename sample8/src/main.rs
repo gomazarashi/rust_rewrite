e74e2b64e79b61f64b7a982a6b269c5c64bdad45
gomazarashi <Seto42357@gmail.com>
2024-10-11 16:17:03 +0900

    小数点が表示されるよう変更

--- sample8/src/main.rs
+++ sample8/src/main.rs
@@ -2,7 +2,7 @@ fn main() {
     let mut inum = 160;
     println!("身長は{}です", inum);
     let fnum = inum as f32;
-    println!("身長は{}です", fnum);
+    println!("身長は{:.1}です", fnum);
     inum = fnum as i32;
     println!("身長は{}です", inum);
 }
