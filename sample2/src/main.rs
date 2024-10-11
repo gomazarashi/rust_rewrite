b619ffa1dfa1e7ac82544422a24f4dd3a6adbd63
gomazarashi <Seto42357@gmail.com>
2024-10-11 10:48:12 +0900

    フォーマッタを適用

--- sample2/src/main.rs
+++ sample2/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     let mut num = 3;
-    println!("変数numの値は{}です",num);
-    num=5;
+    println!("変数numの値は{}です", num);
+    num = 5;
     println!("変数numの値を変更しました");
-    println!("変数numの値は{}です",num)
+    println!("変数numの値は{}です", num)
 }
